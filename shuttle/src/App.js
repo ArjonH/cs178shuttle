70baf187db7c4254a29a4f79c088b46f61d600f3
Prin-P <prinpulkes@gmail.com>
2024-02-28 23:17:46 -0500

    added sidebar with coords displayed

--- shuttle/src/App.js
+++ shuttle/src/App.js
@@ -6,25 +6,38 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicHJpbi1wIiwiYSI6ImNsdDZwZGFpZDBlM2syanA2dmhlb
 export default function App() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
+  const [lng, setLng] = useState(-70.9); //intial location map is zoomed in on
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-    container: mapContainer.current,
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: [lng, lat],
-    zoom: zoom
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [lng, lat],
+        zoom: zoom
+      });
+
+    // Once user interacts with a map, sets the state of the map to these new values
+    map.current.on('move', () => { 
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
     });
   });
 
-    return (
-      <div>
-        <div ref={mapContainer} className="map-container" />
+  return (
+    <div>
+      {/* Displays center coordinates of map */}
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
-    );
+
+      {/* Displays map */}
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
   
 }
 
