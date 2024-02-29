ede639e7e1e591fcc7226dfaf6002c5e279a68e7
Prin-P <prinpulkes@gmail.com>
2024-02-29 00:08:34 -0500

    live data works but not showing

--- shuttle/src/App.js
+++ shuttle/src/App.js
@@ -26,6 +26,78 @@ export default function App() {
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
     });
+
+    map.current.on('load', async () => {
+      // Get the initial location of the shuttle.
+      const geojson = await getLocation();
+      // Add the shuttle location as a source.
+      map.current.addSource('shuttle', {
+          type: 'geojson',
+          data: geojson
+      });
+      // Add the rocket symbol layer to the map.
+      map.current.addLayer({
+          'id': 'shuttle',
+          'type': 'symbol',
+          'source': 'shuttle',
+          'layout': {
+              // This icon is a part of the Mapbox Streets style.
+              // To view all images available in a Mapbox style, open
+              // the style in Mapbox Studio and click the "Images" tab.
+              // To add a new image to the style at runtime see
+              // https://docs.mapbox.com/mapbox-gl-js/example/add-image/
+              'icon-image': 'rocket'
+          }
+      });
+
+      // Update the source from the API every 2 seconds.
+      const updateSource = setInterval(async () => {
+          const geojson = await getLocation(updateSource);
+          map.current.getSource('shuttle').setData(geojson);
+      }, 2000);
+
+      async function getLocation(updateSource) {
+          // Make a GET request to the API and return the location of the ISS.
+          try {
+              const response = await fetch(
+                  'https://passio3.com/harvard/passioTransit/gtfs/realtime/vehiclePositions.json',
+                  { method: 'GET' }
+              );
+              //alert(await response.json())
+              const obj =  await response.json();
+              //alert(JSON.stringify(obj))
+              const entity = obj.entity
+              //alert(entity)
+              const shuttle1 = entity[0] //only get info on first shuttle for now
+              //alert(JSON.stringify(shuttle1))
+              const latitude = shuttle1.vehicle.position.latitude
+              const longitude = shuttle1.vehicle.position.longitude //works
+
+              // Fly the map to the location.
+              /*map.flyTo({
+                  center: [longitude, latitude],
+                  speed: 0.5
+              }); */
+              // Return the location of the ISS as GeoJSON.
+              return {
+                  'type': 'FeatureCollection',
+                  'features': [
+                      {
+                          'type': 'Feature',
+                          'geometry': {
+                              'type': 'Point',
+                              'coordinates': [longitude, latitude]
+                          }
+                      }
+                  ]
+              };
+          } catch (err) {
+              // If the updateSource interval is defined, clear the interval to stop updating the source.
+              if (updateSource) clearInterval(updateSource);
+              throw new Error(err);
+          }
+      }
+    });
   });
 
   return (
