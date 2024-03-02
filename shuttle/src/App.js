503b3123be424dcc9d8af7e0d4841089441b66f5
Prin-P <prinpulkes@gmail.com>
2024-03-02 16:58:17 -0500

    hard coded route showing

--- shuttle/src/App.js
+++ shuttle/src/App.js
@@ -30,7 +30,58 @@ export default function App() {
     map.current.on('load', async () => {
       // Get the initial location of the shuttle.
       const geojson = await getLocation();
+      const geojsonRoutes = await getRouteCoordinates();
 
+      // Route plotting
+      map.current.addSource('route', {
+        'type': 'geojson',
+        'data': {
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {
+                'type': 'LineString',
+                'coordinates': [
+                    [-122.483696, 37.833818],
+                    [-122.483482, 37.833174],
+                    [-122.483396, 37.8327],
+                    [-122.483568, 37.832056],
+                    [-122.48404, 37.831141],
+                    [-122.48404, 37.830497],
+                    [-122.483482, 37.82992],
+                    [-122.483568, 37.829548],
+                    [-122.48507, 37.829446],
+                    [-122.4861, 37.828802],
+                    [-122.486958, 37.82931],
+                    [-122.487001, 37.830802],
+                    [-122.487516, 37.831683],
+                    [-122.488031, 37.832158],
+                    [-122.488889, 37.832971],
+                    [-122.489876, 37.832632],
+                    [-122.490434, 37.832937],
+                    [-122.49125, 37.832429],
+                    [-122.491636, 37.832564],
+                    [-122.492237, 37.833378],
+                    [-122.493782, 37.833683]
+                ]
+            }
+        }
+      });
+      map.current.addLayer({
+          'id': 'route',
+          'type': 'line',
+          'source': 'route',
+          'layout': {
+              'line-join': 'round',
+              'line-cap': 'round'
+          },
+          'paint': {
+              'line-color': '#203333',
+              'line-width': 15
+          }
+      });
+
+
+      // SHUTTLE PLOTTING
       /*
       // Add the shuttle location as a source.
       map.current.addSource('shuttle', {
@@ -84,6 +135,10 @@ export default function App() {
           }
       });*/
 
+      async function getRouteCoordinates(){
+        // Load coordinates from shapes text file
+      }
+
       // Update the source from the API every 2 seconds.
       const updateSource = setInterval(async () => {
           const geojson = await getLocation(updateSource);
@@ -106,13 +161,6 @@ export default function App() {
               //alert(JSON.stringify(shuttle1))
               const latitude = shuttle1.vehicle.position.latitude
               const longitude = shuttle1.vehicle.position.longitude //works
-
-              //Take flyTo out since don't want to follow shuttles
-              // Fly the map to the location.
-              map.current.flyTo({
-                  center: [longitude, latitude],
-                  speed: 0.5
-              }); 
 
               // Return the location of the shuttle as GeoJSON.
               return {
